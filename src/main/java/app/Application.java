e811009b4990243704bd9264dce4757241245e72
Alisson Rosa <alissonrosa3030@gmail.com>
2022-03-03 21:45:50 -0300

    About view

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -1,4 +1,4 @@
-package app.javafxjdbc;
+package app;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -8,22 +8,29 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class Application extends javafx.application.Application {
+
+    private static Scene mainScene;
+
     @Override
     public void start(Stage primaryStage) {
         try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/app/javafxjdbc/gui/MainView.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/app/gui/MainView.fxml"));
             ScrollPane scrollPane = loader.load();
 
             scrollPane.setFitToHeight(true);
             scrollPane.setFitToWidth(true);
 
-            Scene mainScene = new Scene(scrollPane);
+            mainScene = new Scene(scrollPane);
             primaryStage.setScene(mainScene);
             primaryStage.setTitle("Sample JavaFX application");
             primaryStage.show();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static Scene getMainScene(){
+        return mainScene;
     }
 
     public static void main(String[] args) {
